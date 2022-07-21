a911ee96b711fa678d6db0cfbfc7937348ebc1e4
samirich1 <samirrezyapov@mail.ru>
2022-07-21 10:43:49 +0300

    3 задание

---
+++ src/Kfkfk.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Kfkfk {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vvedite size tree");
+        int size = scanner.nextInt();
+        for (int i = 0; i < size; i++) {
+            for (int a = i; a < size - a; a++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
