f402840e8394edb154b67045512c05a7aaebd57a
samirich1 <samirrezyapov@mail.ru>
2022-07-21 10:31:55 +0300

    1 задание

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
         System.out.println("Vvedite size tree");
         int size = scanner.nextInt();
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < i + 1; j++) System.out.print("*");
-            System.out.println(" ");
+            for (int a = i; a < size - a; a++) {
+                System.out.print(" ");
+            }
+                for (int j = 0; j < i + 1; j++)
+                    System.out.print("*");
+                System.out.println(" ");
+            }
         }
     }
-}
